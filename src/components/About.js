a289f740f177b70d9abe4d44f55399ce7decb3dc
Ivan Draganov <dev.advantech@gmail.com>
2023-08-05 17:26:34 +0300

    update about me page

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,8 @@ import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SectionContainer from "../layout/SectionContainer";
 import ProgressBar from "./progressBar";
+import { useState } from "react";
+import { Disclosure } from "@headlessui/react";
 
 const About = () => {
   const circleProgressData = [
@@ -37,6 +39,29 @@ const About = () => {
     },
   ];
 
+  const descriptions = [
+    {
+      title: "Сватбена фотография",
+      content:
+        "- Получавате всички сполучливи кадри, селектирани и обработени от мен. Предадени в онлайн галерия. Aнгажираност - 8 часа. При желание за повече часове се заплаща овъртайм по 80лв/ час -при ангажираност извън гр. Варна - 0,50лв/км",
+    },
+    {
+      title: "Семейна сесия",
+      content:
+        "- Получавате всичи сполучливи кадри в онлайн галерия - неограничен брой/над 100 кадъра/ Селектирани и обработени от мен - 110лв. Времетраене - 60 МИН.",
+    },
+    {
+      title: "Портретна сесия",
+      content:
+        "- Получавате всичи сполучливи кадри в онлайн галерия - неограничен брой/над 100 кадъра/ Селектирани и обработени от мен. ВРЕМЕТРАЕНЕ ДО 60 МИН.",
+    },
+    {
+      title: "Заснемане на ритуал Кръщение",
+      content: "",
+    },
+    // Add other descriptions here
+  ];
+
   return (
     <SectionContainer navName="about">
       <div className="section_inner">
@@ -48,49 +73,41 @@ const About = () => {
               </span>
             </div>
             <div className="wrapper w-full h-auto clear-both float-left flex justify-between mt-[55px]">
-              <div className="left w-[40%]">
+              <div className="left w-[40%] w-full">
                 <p className="mb-[15px]">
-                  Hello there! My name is <strong>Polina Ivanova.</strong> I am a
-                  graphic designer, and I'm very passionate and dedicated to my
-                  work.
+                  Здравейте ! Казвам се Полина и ако четете това , значи че сте
+                  решили заедно да разкажем вашата история в най-чистият й и
+                  съвършен вид! Значи , че ще ми позволите да вникна в във вашия
+                  живот, във вашите мечти , да погледна през вашите очи .
+                  Спомням си , че когато бях дете се възхищавах как е възможно
+                  само с натискането на едно копче да запазиш един миг във
+                  вечността, такъв какъвто е . Да запазиш спомена за хората, за
+                  тези преживявания , които да топлят сърцето ти години наред.
                 </p>
                 <p>
-                  With 20 years experience as a professional a graphic designer,
-                  I have acquired the skills and knowledge necessary to make
-                  your project a success.
+                  Защото за мен това е фотографията, изкуство, начин да спреш
+                  времето , противно на всички теореми и закони . Да спреш
+                  времето там , където си се чувствал щастлив и да осъзнаеш как
+                  с всяка изминала година този спомен става все по-ценен и
+                  по-ценен за теб. Аз няма да ви карам да позирате , да гледате
+                  право към обектива, а напротив…
+                </p>
+                <p>
+                  Ще опитам да ви накарам да погледнете в себе си и в човека до
+                  вас, да бъдете такива каквито сте, щури, естествени и красиви.
+                  Толкова истински , колкото сте сутрин, когато си правите чаша
+                  кафе и вечер, когато мечтаете и се разхождате край брега.
+                  Защото както е казал Бернис Абът ,, Фотографията помага на
+                  хората да видят. “
                 </p>
               </div>
-              <div className="right w-[50%]">
+              {/* <div className="right w-[50%]">
                 <ul>
                   <li className="mb-[8px] w-full float-left">
                     <span className="first inline-block min-w-[100px] font-bold">
                       Name:
                     </span>
-                    <span className="second inline-block">Alan Walker</span>
-                  </li>
-                  <li className="mb-[8px] w-full float-left">
-                    <span className="first inline-block min-w-[100px] font-bold">
-                      Address:
-                    </span>
-                    <span className="second inline-block">
-                      Ave Street, New York, USA
-                    </span>
-                  </li>
-                  <li className="mb-[8px] w-full float-left">
-                    <span className="first inline-block min-w-[100px] font-bold">
-                      Study:
-                    </span>
-                    <span className="second inline-block">
-                      Univercity of Oxford
-                    </span>
-                  </li>
-                  <li className="mb-[8px] w-full float-left">
-                    <span className="first inline-block min-w-[100px] font-bold">
-                      Degree:
-                    </span>
-                    <span className="second inline-block">
-                      Master of Science
-                    </span>
+                    <span className="second inline-block">Полина Иванова</span>
                   </li>
                   <li className="mb-[8px] w-full float-left">
                     <span className="first inline-block min-w-[100px] font-bold">
@@ -101,7 +118,7 @@ const About = () => {
                         className="text-[#7d7789] transition-all duration-300 hover:text-[#333]"
                         href="#"
                       >
-                        hello@cavani.com
+                        poli110356@gmail.com
                       </a>
                     </span>
                   </li>
@@ -114,7 +131,7 @@ const About = () => {
                     </span>
                   </li>
                 </ul>
-              </div>
+              </div> */}
             </div>
           </div>
           <div className="services w-full h-auto clear-both float-left mb-[87px]">
@@ -127,46 +144,46 @@ const About = () => {
                 </div>
                 <div className="list w-full h-auto clear-both float-left mt-[55px]">
                   <ul>
-                    <li className="mb-[8px] w-full float-left relative pl-[25px]">
-                      Web Development
-                    </li>
-                    <li className="mb-[8px] w-full float-left relative pl-[25px]">
-                      Graphic Design
-                    </li>
-                    <li className="mb-[8px] w-full float-left relative pl-[25px]">
-                      Landing Page
-                    </li>
-                    <li className="mb-[8px] w-full float-left relative pl-[25px]">
-                      On-Page SEO
-                    </li>
-                    <li className="w-full float-left relative pl-[25px]">
-                      Web Hosting
-                    </li>
+                    {descriptions.map((item, index) => (
+                      <Disclosure
+                        as="li"
+                        className="mb-[8px] w-full float-left relative pl-[25px]"
+                        key={index}
+                      >
+                        {({ open }) => (
+                          <>
+                            <Disclosure.Button className="text-xl">
+                              {item.title}
+                            </Disclosure.Button>
+                            <Disclosure.Panel>
+                              <p>{item.content}</p>
+                            </Disclosure.Panel>
+                          </>
+                        )}
+                      </Disclosure>
+                    ))}
                   </ul>
                 </div>
               </div>
               <div className="service_list w-[50%]">
                 <div className="cavani_tm_title w-full h-auto clear-both float-left overflow-hidden">
                   <span className="inline-block relative font-poppins text-[#333] uppercase font-bold tracking-[8px]">
-                    Interests
+                    Prices
                   </span>
                 </div>
                 <div className="list w-full h-auto clear-both float-left mt-[55px]">
                   <ul>
                     <li className="mb-[8px] w-full float-left relative pl-[25px]">
-                      Painting &amp; Drawing
+                      100лв/час
                     </li>
                     <li className="mb-[8px] w-full float-left relative pl-[25px]">
-                      Reading &amp; Writing
+                      110лв/час
                     </li>
                     <li className="mb-[8px] w-full float-left relative pl-[25px]">
-                      Music &amp; Cinema
+                      100лв/60 мин
                     </li>
                     <li className="mb-[8px] w-full float-left relative pl-[25px]">
-                      Travel &amp; Picnik
-                    </li>
-                    <li className="w-full float-left relative pl-[25px]">
-                      Rain &amp; Snow
+                      120лв/час
                     </li>
                   </ul>
                 </div>
