33942c7e37b21a9d0e59c1b86fad01d7282fd869
Ivan Draganov <dev.advantech@gmail.com>
2023-08-17 15:55:40 +0300

    add localization for vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next-i18next').UserConfig} */
+const path = require('path')
 
 module.exports = {
   i18n: {
     defaultLocale: 'bg',
     locales: ['bg', 'en'],
+    localePath: path.resolve('./public/static/locales')
   },
   fallbackLng: {
     default: ['bg'],
