dc737bb5e225830f4fa4dbf36c9197db4b294b3b
Ivan Draganov <dev.advantech@gmail.com>
2023-08-21 14:35:06 +0300

    polinas photo will show only on about page

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import Service from "@/src/components/Service";
 import Layout from "@/src/layout/Layout";
 import dynamic from "next/dynamic";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 const Portfolio = dynamic(() => import("@/src/components/Portfolio"), {
   ssr: false,
 });
@@ -14,14 +15,35 @@ const Home = dynamic(() => import("@/src/components/Home"), {
 });
 
 const Index = () => {
+  const [imageUrl, setImageUrl] = useState("/assets/img/about/1.jpg"); // Default image
+
+  useEffect(() => {
+    const checkHash = () => {
+      if (window.location.hash === '#about') {
+        setImageUrl("/assets/img/about/poliprofilna.webp");
+      } else {
+        setImageUrl("/assets/img/about/1.jpg");
+      }
+    };
+
+    checkHash();
+
+    window.addEventListener('hashchange', checkHash);
+
+    return () => {
+      window.removeEventListener('hashchange', checkHash);
+    };
+  }, []);
+
   return (
     <Layout>
       <div className="cavani_tm_mainpart absolute inset-[70px] overflow-hidden middle:inset-x-0 middle:bottom-0 middle:top-[55px]">
         <div className="author_image absolute top-0 left-0 bottom-0 w-[40%] z-[15]">
           <div
             className="main absolute inset-0 bg-no-repeat bg-cover"
-            data-img-url="assets/img/about/poliprofilna.webp"
+            style={{ backgroundImage: `url(${imageUrl})` }}
           />
+          {/* <img src={imageUrl} className="main absolute inset-0 bg-no-repeat bg-cover" /> */}
         </div>
         <div className="main_content absolute top-0 right-0 bottom-0 w-[60%]">
           {/* HOME */}
