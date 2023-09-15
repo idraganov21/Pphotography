06b2215d09d7f7ee2c99d2501b92049b99393156
Ivan Draganov <dev.advantech@gmail.com>
2023-09-16 00:42:36 +0300

    revert

--- pages/api/staticdata.js
+++ pages/api/staticdata.js
@@ -2,15 +2,10 @@ import path from 'path';
 import { promises as fs } from 'fs';
 
 export default async function handler(req, res) { 
-  // Find the absolute path of the json directory
+  //Find the absolute path of the json directory
   const jsonDirectory = path.join(process.cwd(), 'public/json');
-
-  // Read the json data file data.json
+  //Read the json data file data.json
   const fileContents = await fs.readFile(jsonDirectory + '/cloudinaryImages.json', 'utf8');
-
-  // Convert the string into a JSON object
-  const jsonData = JSON.parse(fileContents);
-
-  // Return the content of the data file in json format
-  res.status(200).json(jsonData);
+  //Return the content of the data file in json format
+  res.status(200).json(fileContents);
 }
