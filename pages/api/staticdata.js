c2c7c7997ab2f4b31cad0fa707a422337a9b3a88
Ivan Draganov <dev.advantech@gmail.com>
2023-09-16 00:38:26 +0300

    trying something

--- pages/api/staticdata.js
+++ pages/api/staticdata.js
@@ -2,10 +2,15 @@ import path from 'path';
 import { promises as fs } from 'fs';
 
 export default async function handler(req, res) { 
-  //Find the absolute path of the json directory
+  // Find the absolute path of the json directory
   const jsonDirectory = path.join(process.cwd(), 'public/json');
-  //Read the json data file data.json
+
+  // Read the json data file data.json
   const fileContents = await fs.readFile(jsonDirectory + '/cloudinaryImages.json', 'utf8');
-  //Return the content of the data file in json format
-  res.status(200).json(fileContents);
+
+  // Convert the string into a JSON object
+  const jsonData = JSON.parse(fileContents);
+
+  // Return the content of the data file in json format
+  res.status(200).json(jsonData);
 }
